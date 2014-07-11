48bf1fe67fbf988c7f6a45c1da391605106f7d32
Ivo D. Shterev <i.shterev@duke.edu>
2014-07-11 00:00:00 +0000

    version 0.14.6

--- R/test.permgpu.R
+++ R/test.permgpu.R
@@ -1,30 +1,37 @@
 test.permgpu = function(test){
-  otime = rep(1,20)/seq(1,20)
-  event = rep(c(0,1), 10)
+  n = 20
+  K = 10
+  otime = rep(1,n)/seq(1,n)
+  event = rep(c(0,1), n/2)
   pdat = data.frame(otime, event)
-  expr = matrix(seq(1,40)/seq(40,1), 2, 20)
+  expr = matrix(seq(1,K*n)/seq(K*n,1), K, n)
 
-  rownames(expr) = paste("g", 1:2, sep="")
-  patid = paste("id", 1:20, sep="")
+  rownames(expr) = paste("g", 1:K, sep="")
+  patid = paste("id", 1:n, sep="")
   rownames(pdat) = patid
   colnames(expr) = patid
 
   testdat = makeExprSet(expr, pdat)
 
-  if (test == "ttest")
-    checkEquals(sum(permgpu(testdat, "event", B=0, test="ttest")[["stat"]]), -1.439, tolerance=0.001)
+  if (test == "ttest"){
+    sumstat = sum(sapply(1:K, function(k){t.test(x~y, data=data.frame(x=exprs(testdat)[k,], y=pData(testdat)$event))$statistic}))
+    checkEquals(sum(permgpu(testdat, "event", B=0, test="ttest")[["stat"]]), sumstat, tolerance=0.001)
+  }
   else if (test == "wilcoxon"){
     n0 = sum(event==0)
     n1 = sum(event==1)
     m  = n1 * (n0+n1+1.0) / 2.0
     std = sqrt(n0*n1*(n0+n1+1.0)/12.0)
-    checkEquals(sum(permgpu(testdat, "event", B=0, test="wilcoxon")[["stat"]]), sum((sum(rank(exprs(testdat)[1,])[event==1])-m)/std, (sum(rank(exprs(testdat)[2,])[event==1])-m)/std), tolerance=0.001)
+    sumstat = sum(sapply(1:K, function(k){(sum(rank(exprs(testdat)[k,])[event==1])-m)/std}))
+    checkEquals(sum(permgpu(testdat, "event", B=0, test="wilcoxon")[["stat"]]), sumstat, tolerance=0.001)
   }
   else if (test == "npcox"){
-    checkEquals(sum(permgpu(testdat, "otime", "event", B=0, test="npcox")[["stat"]]), 39.21569, tolerance=0.001)
+    sumstat = sum(sapply(1:K, function(k){summary(coxph(Surv(otime,event)~x, data=data.frame(x=rank(exprs(testdat)[k,]),otime=pData(testdat)[["otime"]], event=pData(testdat)[["event"]])))[["sctest"]][1]}))
+    checkEquals(sum(permgpu(testdat, "otime", "event", B=0, test="npcox")[["stat"]]), sumstat, tolerance=0.001)
   }
   else if (test == "cox"){
-    checkEquals(sum(permgpu(testdat, "otime", "event", B=0, test="cox")[["stat"]]), 57.21867, tolerance=0.001)
+    sumstat = sum(sapply(1:K, function(k){summary(coxph(Surv(otime,event)~x, data=data.frame(x=exprs(testdat)[k,],otime=pData(testdat)[["otime"]], event=pData(testdat)[["event"]])))[["sctest"]][1]}))
+    checkEquals(sum(permgpu(testdat, "otime", "event", B=0, test="cox")[["stat"]]), sumstat, tolerance=0.001)
   }
   else
     stop("test unknown.")
--- R/test.scoregpu.R
+++ R/test.scoregpu.R
@@ -1,6 +1,10 @@
 test.scoregpu = function(){
-  outcome = rep(1,20)/10
-  event = rep(c(0,1), 10)
-  expr = matrix(seq(1,40), 2, 20)
-  checkEquals(sum(scoregpu(outcome, event, expr, test="npcox", B = 0, index = FALSE)), 1.567855, tolerance=0.001)
+  n = 20
+  K = 10
+  otime = rep(1,n)/seq(1,n)
+  event = rep(c(0,1), n/2)
+  expr = matrix(seq(1,K*n)/seq(K*n,1), K, n)
+
+  sumstat = sum(sapply(1:K, function(k){summary(coxph(Surv(otime,event)~x, data=data.frame(x=rank(expr[k,]),otime, event)))[["sctest"]][1]}))
+  checkEquals(sum(scoregpu(otime, event, expr, test="npcox", B = 0, index = FALSE, pval=FALSE)), sumstat, tolerance=0.001)
 }
